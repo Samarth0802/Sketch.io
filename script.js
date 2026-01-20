5e49b6c647808b7ac666010aca4bd16883a61cad
Samarth Varshney <samarthvarshney08022005@gmail.com>
2026-01-20 23:16:04 +0530

    Created the Welcome Page

---
+++ script.js
@@ -0,0 +1,21 @@
+const fullPage = document.querySelector('.full-page')
+const extraHeading = document.querySelector('h5')
+const enterBtn = document.querySelector('.enter-btn')
+
+enterBtn.addEventListener('click',()=>{
+    fullPage.classList.add('out')
+    extraHeading.classList.add('out')
+    setTimeout(() => {
+        window.location.href = 'index2.html'  //Like this added beacuse the animation will be missed otherwise.
+    }, 900);
+})
+
+document.addEventListener('keydown',(e)=>{
+    if(e.key==="Enter"){
+        fullPage.classList.add('out')
+        extraHeading.classList.add('out')
+        setTimeout(()=>{
+            window.location.href = 'index2.html'
+        },500)
+    }   
+})
